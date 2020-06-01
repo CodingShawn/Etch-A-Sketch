e08312e960f5df0c0b84363e2ec2cecd740254d5
Shawn <codingshawn@gmail.com>
2020-06-01 23:10:33 +0800

    Added functionality for grids to turn black after mouse hover

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ const stylesheet = document.styleSheets[0];
 function createGrid(width) {
     for (let i = 0; i < width * width; i++) {
         let gridDiv = document.createElement('div');
-        gridDiv.classList.add('grid')
+        gridDiv.classList.add('grid');
         sketchbox.appendChild(gridDiv);
     }
     stylesheet.insertRule(`#sketchbox {grid-template-columns: repeat(${width}, 1fr); 
                                     grid-template-rows: repeat(${width}, 1fr)}`, 0);
+    gridSetUp();
+}
+
+function gridSetUp() {
+    const grids = Array.from(document.querySelectorAll('.grid'));
+    grids.forEach(grid => grid.addEventListener('mouseover', gridChangeColor));
+}
+
+function gridChangeColor() {
+    this.classList.add('gridAfterHover')
 }
 
 createGrid(16);
